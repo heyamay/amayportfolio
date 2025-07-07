ab4929680b69a42d0079023f6180f702a240b868
Amay Jaiswal <amayjaiswal@gmail.com>
2025-07-08 01:20:26 +0530

    Updated experience section and resume

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -19,18 +19,30 @@ const Experience = () => {
         <div className="mt-6 w-full h-auto border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
 
         <ResumeCard
-            title="System Engineer"
-            subTitle="Infosys Limited (Starting July 2025)- Mysuru"
+            title="Cloud DevOps Engineer"
+            subTitle="BUSINESSNEXT (July 2025)- Noida"
             result=""
             des={
               <>
-                ● I will undergo extensive training at Infosys's Mysore campus.<br />
-                ● India's largest IT training centre, designed to prepare freshers for advanced technical, soft skills, and domain-specific requirements.<br />
+                ● Worked on deploying and managing cloud infrastructure using AWS services like EC2, S3, Lambda, and CloudFormation.<br />
+                ● Automated CI/CD pipelines using Jenkins for efficient build, test, and deployment processes.<br />
                 
               </>
             }
           />
-
+          <ResumeCard
+            title="DevOps Engineer"
+            subTitle="Perception & Quant Research (April 2025 - July 2025) - Remote"
+            result=""
+            des={
+              <>
+                ● Built and maintained Docker containerized apps using Docker Compose for multi-container setups and optimized Dockerfiles to reduce build time and environment issues.<br />
+                ● Implemented real-time centralized log monitoring using Promtail, Loki, and Grafana across multiple Linux servers, enabling live log streaming, filtering, and visualization from remote nodes in a unified dashboard.
+<br />
+                ● Designed and implemented Bitbucket CI/CD pipelines for various projects, integrating DevSecOps practices for secure and automated deployments.<br />
+              </>
+            }
+          />
           <ResumeCard
             title="AWS Cloud & DevOps Intern"
             subTitle="WebKalakaar Software Private Limited (October 2024 - February 2025) - Jaipur"
@@ -42,29 +54,6 @@ const Experience = () => {
                 ● Contributed to the development of CI/CD pipelines using Jenkins, reducing deployment time by 40%.<br />
                 ● Collaborated with senior developers to integrate AWS services (CloudFront, IAM, API Gateway) and REST
                 APIs into existing systems, improving API response efficiency by 35%<br />
-              </>
-            }
-          />
-          <ResumeCard
-            title="Data Analysis Intern"
-            subTitle="Cognifyz Technologies (August 2024 - September 2024) - NOIDA"
-            result=""
-            des={
-              <>
-                ● Analyzed a 1000+ restaurant dataset to identify top cuisines, city trends, and price ranges.<br />
-                ● Assessed online delivery and 500+ ratings, boosting customer satisfaction by 15%.<br />
-                ● Explored 30+ cuisine combinations, enhancing menu performance and engagement.<br />
-                ● Mapped locations and evaluated 50+ restaurant chains for popularity.<br />
-                ● Used Python (Pandas, Matplotlib, Seaborn) for data analysis and visualization.<br />
-                ● View Project:{" "}
-                <a
-                  href="https://github.com/heyamay/Consumer-Trends-and-Preferences-in-the-Restaurant-Industry"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-500 underline"
-                >
-                  Consumer Trends and Preferences in the Restaurant Industry
-                </a>
               </>
             }
           />
